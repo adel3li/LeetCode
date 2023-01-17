894a345f8aac53918faa91ef94230820d3ccc894
adel3li <92190398+adel3li@users.noreply.github.com>
2023-01-17 22:48:11 +0200

    Update 191. Number of 1 Bits.py

--- Programming Skills/191. Number of 1 Bits.py
+++ Programming Skills/191. Number of 1 Bits.py
@@ -2,5 +2,7 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count('1')
 
-    
-    
+#  bin() is an in-built function in Python that takes in integer x and returns the binary representation of x in a string format.
+# If x is not an integer,
+# then the _index()_ method needs to be implemented to obtain an integer as the return value instead of as a “TypeError” exception.
+# source(Educative).
