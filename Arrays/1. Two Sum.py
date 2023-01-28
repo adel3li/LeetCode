e35766d31cc83f990511ae4d239db7834a98f7d9
adel3li <92190398+adel3li@users.noreply.github.com>
2023-01-28 18:15:29 +0200

    Update 1. Two Sum.py

--- Arrays/1. Two Sum.py
+++ Arrays/1. Two Sum.py
@@ -1,3 +1,6 @@
 class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums * 2
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range (0,len(nums)):
+            for j in range (i+1,len(nums)):
+                if nums[i] + nums[j] == target :
+                    return [i,j]
