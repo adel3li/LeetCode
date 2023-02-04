b56028f3a87f62ee512bc1980c99b7986f74cc43
adel3li <92190398+adel3li@users.noreply.github.com>
2023-02-04 14:50:34 +0200

    Update 912. Sort an Array.py

--- Sorting/912. Sort an Array.py
+++ Sorting/912. Sort an Array.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        d, n ,m ,s = collections.Counter(nums), min(nums), max(nums), []
+        for i in range(n, m+1):
+            s.extend([i]*d[i])
+        return s
